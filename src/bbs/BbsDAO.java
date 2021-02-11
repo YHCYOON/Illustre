98e962abf19eeb5868f25525b26bfbee94edebb7
YHCYOON <yhc920923@gmail.com>
2021-02-11 13:11:16 +0900

    글 작성 메서드

--- src/bbs/BbsDAO.java
+++ src/bbs/BbsDAO.java
@@ -1,5 +1,79 @@
 package bbs;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class BbsDAO {
 
+	Connection conn;
+	
+	// MySQL 접속
+	public BbsDAO() {
+		try {
+			String dbURL = "jdbc:mysql://localhost:3306/Illustre";
+			String dbID = "root";
+			String dbPassword = "root";
+			Class.forName("com.mysql.jdbc.Driver");	
+			conn = DriverManager.getConnection(dbURL, dbID, dbPassword);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	// 현재 날짜와 시간 반환 메서드
+	public String getDate() {
+		PreparedStatement pstmt;
+		ResultSet rs;
+		String SQL = "SELECT NOW()";
+		try {
+			pstmt = conn.prepareStatement(SQL);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				return rs.getString(1);
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return null;	//데이터베이스 오류
+	}
+	
+	// 글 번호 반환 메서드 (현재 게시글 개수 +1 반환)
+	public int getBbsID() {
+		PreparedStatement pstmt;
+		ResultSet rs;
+		String SQL = "SELECT bbsID FROM BBS ORDER BY bbsID DESC LIMIT 1";
+		try {
+			pstmt = conn.prepareStatement(SQL);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				return rs.getInt(1) +1 ;	// 현재 게시글 개수 +1
+			}
+			return 1;	// 현재가 첫번째 글인 경우
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		return -1;	// 데이터베이스 오류
+	}
+	
+	// 글쓰기 메서드
+	public int write(String bbsTitle, String bbsContent, String userID) {
+		PreparedStatement pstmt;
+		String SQL = "INSERT INTO BBS VALUES (?, ?, ?, ?, ?, ?, ?)";
+		try {
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setInt(1, getBbsID());
+			pstmt.setString(2, bbsTitle);
+			pstmt.setString(3, bbsContent);
+			pstmt.setString(4, userID);
+			pstmt.setString(5, getDate());
+			pstmt.setInt(6, 0);
+			pstmt.setInt(7, 1);
+			return pstmt.executeUpdate();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return -1;	//데이터베이스 오류
+	}
 }
